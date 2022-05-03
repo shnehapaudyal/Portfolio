12ad84feb299d3b65c8310741b06161a5dae90bd
shneha.paudyal <eshnehapaudyal@gmail.com>
2022-05-03 12:09:37 +0545

    Content changes

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -1,17 +1,17 @@
 export const MenuItems = [
     {
-        title: 'Home',
-        url: '/',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Work',
-        url: '/work',
-        cName: 'nav-links'
-    },
-    {
-        title: 'About',
-        url: '/about',
-        cName: 'nav-links'
+//        title: 'Home',
+//        url: '/',
+//        cName: 'nav-links'
+//    },
+//    {
+//        title: 'Work',
+//        url: '/work',
+//        cName: 'nav-links'
+//    },
+//    {
+//        title: 'About',
+//        url: '/about',
+//        cName: 'nav-links'
     }
 ]
