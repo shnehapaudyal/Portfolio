2599d69e4f014dd748d4a60631400043eea5def8
shneha.paudyal <eshnehapaudyal@gmail.com>
2022-05-03 15:10:27 +0545

    photo added

--- src/components/Hero/Graphics/Graphics.jsx
+++ src/components/Hero/Graphics/Graphics.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-// import Photo from '../../../Assets/Photo.jpg'
+import Photo from '../../../Assets/photo.JPG'
 import './Graphics.styles.css'
 export default function Graphics() {
     return (
         <div className="container-p">
         <div className="image-cropper ">
-{/*             <img src={Photo} alt="Logo" className="image" /> */}
+            <img src={Photo} alt="Logo" className="image" />
         </div>
         </div>
     )
