d65a286338a746d89c3d7fd3e13fedaf5328418c
shneha.paudyal <eshnehapaudyal@gmail.com>
2022-05-03 17:24:34 +0545

    Theme changes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const links = {
     firstName:'Shneha',
     lastName:'Paudyal',
-    github:'https://github.com/Niraj22',
-    twitter:'https://twitter.com/niraj614',
-    email:"nirajbudhathoki3@gmail.com",
-    repoUrl:"https://api.github.com/users/niraj22/repos"
+    github:'https://github.com/shnehapaudyal',
+    twitter:'https://twitter.com/shneha',
+    email:"eshnehapaudyal@gmail.com",
+    repoUrl:"https://api.github.com/users/shnehapaudyal/repos"
 }
